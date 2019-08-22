b0875c5649b81cb46c37b6d42fa6debe97506f05
Kristie Chow <kristie@gotenzo.com>
2019-08-22 14:26:33 +0700

    Update home

---
+++ js/isView.js
@@ -0,0 +1,16 @@
+import $ from 'jquery'
+import 'is-in-viewport'
+
+jQuery(document).ready(function($) {
+
+
+	var ns_in_viewport = function() {
+		// Trigger transition on scroll for each content-row
+		$('section.content-row').each(function(){
+			$(this).isInViewport({ tolerance: -100 }).addClass('in-view');
+		});
+	}
+	ns_in_viewport();
+
+	$(window).scroll(ns_in_viewport);
+});
